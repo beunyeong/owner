f18de7dc80353af8aae5de1c439fd0f2430792e8
MinSeok3796 <ms3796@naver.com>
2024-12-20 19:32:00 +0900

    generate project

---
+++ src/main/java/com/example/oner/OnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.oner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnerApplication.class, args);
+    }
+
+}
